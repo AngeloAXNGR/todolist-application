87792a64361a2c563f007b82bfdbbf944c16c68c
LuhxEn <axngrsantos@gmail.com>
2023-01-11 07:35:20 +0800

    Create add and delete function for tasks

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { signInWithEmailAndPassword, onAuthStateChanged, createUserWithEmailAndPassword, signOut} from 'firebase/auth';
 import { auth, db} from '../firebase_config';
 import {set, ref, onValue, remove, update} from 'firebase/database';
-import { useNavigate } from 'react-router-dom';
 import { nanoid } from "nanoid";
 const TodoContext = React.createContext();
 
@@ -10,8 +8,11 @@ export const TodoProvider = ({children}) =>{
 
 const [todo, setTodo] = React.useState([]);
 const [projectForm, setProjectForm] = React.useState({projectTitle: ''});
+const [showForm, setShowForm] = React.useState(false);
 const [selected, setSelected] = React.useState(null);
+const [active, setActive] = React.useState(null);
 
+const [tasks, setTasks] = React.useState([]);
 
 
 React.useEffect(() => {
@@ -29,59 +30,110 @@ React.useEffect(() => {
       onValue(ref(db, `/${auth.currentUser.uid}`), snapshot =>{
         setTodo([]);
         const data = snapshot.val();
-        console.log(data);
+        // console.log(data);
         if(data !==null){
           renderData(data);
         }
       })
     } 
   })
-}, [])
+  }, [])
 
 
-const handleProjectForm = (event) => {
-  const {name, type, checked, value} = event.target;
-  setProjectForm(prevForm => {
-    return {...prevForm,
-      [name] : type === "checkbox" ? checked : value
+  React.useEffect(() => {
+    const activeProject = todo.find(project => project.projectId === active)
+    if(activeProject === undefined){
+      setTasks([])
+    }else{
+      setTasks(activeProject.tasks)
     }
-  })
-}
+    // console.log(tasks)
+  }, [todo, active])
 
-const addProject = () =>{
-  if(projectForm.projectTitle.trim() === ''){
-    alert('A project must include a name');
-    return;
+  const handleProjectForm = (event) => {
+    const {name, type, checked, value} = event.target;
+    setProjectForm(prevForm => {
+      return {...prevForm,
+        [name] : type === "checkbox" ? checked : value
+      }
+    })
   }
 
-  const projectId = nanoid()
-  set(ref(db, `${auth.currentUser.uid}/${projectId}`),
-    {
-      projectId: projectId,
-      projectTitle: projectForm.projectTitle,
-      tasks: JSON.stringify([{taskId: nanoid(), taskTitle: 'Test Task'}]),
+  const addProject = () =>{
+    if(projectForm.projectTitle.trim() === ''){
+      alert('A project must include a name');
+      return;
     }
-  )
-  setProjectForm({projectTitle: ''});
-}
+
+    const projectId = nanoid()
+    set(ref(db, `${auth.currentUser.uid}/${projectId}`),
+      {
+        projectId: projectId,
+        projectTitle: projectForm.projectTitle,
+        // tasks: JSON.stringify([{taskId: nanoid(), taskTitle: 'Test Task'}]),
+        tasks: JSON.stringify([]),
+      }
+    )
+    setProjectForm({projectTitle: ''});
+    setShowForm(false);
+  }
 
 
-const deleteProject = (projectId) => {
-  remove(ref(db, `/${auth.currentUser.uid}/${projectId}`))
-}
+  const deleteProject = (projectId) => {
+    remove(ref(db, `/${auth.currentUser.uid}/${projectId}`))
+    setActive(null);
+  }
 
-const updateProject = (projectId) =>{
-  const selectedProject = todo.find(project => project.projectId === projectId)
-  update(ref(db, `/${auth.currentUser.uid}/${projectId}`),
-    {
-      projectId: projectId,
-      projectTitle: projectForm.projectTitle,
-      tasks: JSON.stringify(selectedProject.tasks)
+
+  const setEdit = (projectId, projectTitle) =>{
+    setSelected(projectId)
+    setProjectForm({projectTitle: projectTitle})
+  }
+
+  const updateProject = (projectId) =>{
+    if(projectForm.projectTitle.trim() === ''){
+      alert('A project must include a name');
+      return;
     }
-  )
-  setSelected(null);
-  setProjectForm({projectTitle:''})
-}
+
+    const selectedProject = todo.find(project => project.projectId === projectId)
+    update(ref(db, `/${auth.currentUser.uid}/${projectId}`),
+      {
+        projectId: projectId,
+        projectTitle: projectForm.projectTitle,
+        tasks: JSON.stringify(selectedProject.tasks)
+      }
+    )
+    setSelected(null);
+    setProjectForm({projectTitle:''})
+  }
+
+
+  const addTaskToProject = (taskTitle) => {
+    const activeProject = todo.find(project => project.projectId === active)
+    console.log(activeProject);
+    
+    const newTaskArray = activeProject.tasks
+    newTaskArray.push({taskId:nanoid(), taskTitle:taskTitle})
+    update(ref(db, `/${auth.currentUser.uid}/${active}`),
+    {
+      projectId: active,
+      projectTitle: activeProject.projectTitle,
+      tasks: JSON.stringify(newTaskArray)
+    })
+  }
+
+  const deleteTask = (taskId) => {
+    const activeProject = todo.find(project => project.projectId === active)
+    const newTaskArray = activeProject.tasks.filter(task => task.taskId !== taskId);
+    
+    update(ref(db, `/${auth.currentUser.uid}/${active}`),
+    {
+      projectId: active,
+      projectTitle: activeProject.projectTitle,
+      tasks: JSON.stringify(newTaskArray)
+    })
+  }
 
 
   return(
@@ -94,8 +146,16 @@ const updateProject = (projectId) =>{
           addProject,
           deleteProject,
           selected,
-          setSelected,
-          updateProject
+          setEdit,
+          updateProject,
+          showForm,
+          setShowForm,
+          active,
+          setActive,
+          tasks,
+          setTasks,
+          addTaskToProject,
+          deleteTask
         }
       }
     >

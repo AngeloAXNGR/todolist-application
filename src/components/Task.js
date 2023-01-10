54c977a84b90fdca9afb31003fe3d114194420bc
LuhxEn <axngrsantos@gmail.com>
2023-01-11 07:34:36 +0800

    Create task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,38 @@
 import React from 'react'
-
+import TodoContext from '../contexts/TodoContext'
 export default function Task() {
+  const {tasks, addTaskToProject, deleteTask} = React.useContext(TodoContext);
+  const [taskForm, setTaskForm] = React.useState({taskTitle:''})
+
+  const taskElements = tasks.map(task => {
+    return(
+      <div className="task">
+        {task.taskTitle}
+        <button onClick={() => {deleteTask(task.taskId)}}>Delete Task</button>
+      </div>
+    )
+  })
+  const handleTaskForm = (event) =>{
+    const {name, type, checked, value} = event.target;
+
+    setTaskForm(prevForm => {
+      return {...prevForm,
+        [name] : type === "checkbox" ? checked : value
+      }
+    })
+  }
+
+  console.log(taskForm);
   return (
-    <div className='tasks'></div>
+    <div className='tasks'>
+      <input 
+        type="text" 
+        name="taskTitle"
+        value={taskForm.taskTitle}
+        onChange={handleTaskForm}
+      />
+      <button onClick={() => addTaskToProject(taskForm.taskTitle)}>Add Task</button>
+      <div className="task-list">{taskElements}</div>
+    </div>
   )
 }
